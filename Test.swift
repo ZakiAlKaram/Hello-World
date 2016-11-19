0fdc4bc9eac4591420ef610af769c6c7abf03cf3
ZakiRKaram <zakialkaram@hotmail.com>
2016-11-19 10:18:49 +0300

    Update Test.swift

--- Test.swift
+++ Test.swift
@@ -1,17 +1,1 @@
-            if selectedIncome != nil {
-                //check if the selectedIncome deleted
-                selectedIncome = Income.fetchOne(db, key: selectedIncome!.id!)
-                if selectedIncome == nil {
-                    selectedIncome = Income.order(Column("recievedDate").desc).limit(1).fetchOne(db)
-                }
-            }
-            else {
-                //just open the app
-                let lastSelectedIncomeId = defaults.integer(forKey: "lastSelectedIncomeId")
-                if lastSelectedIncomeId > 0 {
-                    selectedIncome = Income.fetchOne(db, key: lastSelectedIncomeId)
-                }
-                if (selectedIncome == nil) {
-                    selectedIncome = Income.order(Column("recievedDate").desc).limit(1).fetchOne(db)
-                }
-            }
+
